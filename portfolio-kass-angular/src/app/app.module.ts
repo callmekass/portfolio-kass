4b22ecb28c7c34c206863944688fc022baf97db8
Kassidy <kassidyjo97@gmail.com>
2023-12-10 20:19:05 -0600

    create basic navbar

--- portfolio-kass-angular/src/app/app.module.ts
+++ portfolio-kass-angular/src/app/app.module.ts
@@ -3,12 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FooterComponent } from './shared/footer/footer.component';
-import { HeaderComponent } from './shared/header/header.component';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
-  declarations: [AppComponent, FooterComponent, HeaderComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, SharedModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- portfolio-kass-angular/src/app/shared/header/header.component.ts
+++ portfolio-kass-angular/src/app/shared/header/header.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import {} from 'ngx-bootstrap/';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
+  navbarOpen = false;
 
+  toggleNavbar() {
+    this.navbarOpen = !this.navbarOpen;
+  }
 }
